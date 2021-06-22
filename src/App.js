b5061511a74935e8080f93b4bd29185b4879ad38
plague <plague@outlook.es>
2021-06-21 21:03:07 -0400

    app breaking bad

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Quote from "./components/Quote";
+import Spinner from "./components/Spinner";
 
+const initialState = {
+  text:'',
+  author:''
+}
 function App() {
+  const [quote, setQuote] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+  
+  const getQuote = async() => {
+    setLoading(true);
+    const url = "https://www.breakingbadapi.com/api/quote/random";
+    const res = await fetch(url)
+    const [data] = await res.json();
+    const {quote:text, author} = data;
+    setQuote({
+      text,
+      author
+    })
+    setLoading(false);
+  }
+  useEffect(() => {
+    getQuote()
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg" alt='logo of breaking bad'/>
+        <button onClick={()=> getQuote()}>Get Another</button>
+        {loading 
+          ? <Spinner/>:
+          <Quote quote={quote}/>
+        }
     </div>
   );
 }
---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Quote = ({quote}) => {
+    const {text, author} = quote;
+    return (
+        <p>
+          {text} <br />
+          <span>{author}</span>
+        </p>
+    )
+}
+
+export default Quote
